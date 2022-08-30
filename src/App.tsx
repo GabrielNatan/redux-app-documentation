9051377e603dc9a1103dc9c4524a29e1d3d2d3a4
gabriel natan <g.souza@grouplinknetwork.com>
2022-08-30 00:01:50 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { useAppDispatch, useAppSelector} from './app/hooks'
+import { incremented,amountAdded } from "./features/counter/counter-slice"
+import { useFetchBreedsQuery } from './features/dogs/dogs-api-slice';
 
 function App() {
+  const count = useAppSelector((state)=> state.counter.value);
+  const dispatch = useAppDispatch();
+
+  const [numDogs, setNumDogs] = useState(10)
+
+  const { data = [], isFetching } = useFetchBreedsQuery(numDogs);
+
+  function handleClick(){
+    dispatch(amountAdded(3))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      {count}
+      <button onClick={()=>{handleClick()}} >
+          counter
+      </button>
+
+      <div>
+        <p>Dogs to fetch:</p>
+        <select value={numDogs} onChange={(e)=>{setNumDogs(Number(e.target.value))}}>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="15">15</option>
+          <option value="20">20</option>
+          <option value="25">25</option>
+        </select>
+      </div>
+
+      <div>
+          <p>Number of dogs fetched: {data.length}</p>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Picture</th>
+          </tr>
+        </thead>
+        <tbody>
+              {
+                data.map((breed)=>{
+                return(
+                  <tr key={breed.id}>
+                  <td>{breed.name}</td>
+                  <td>
+                    <img src={breed.image.url} alt={breed.name} height={250}/>
+                  </td>
+                </tr>
+                )
+                })
+              }
+        </tbody>
+      </table>
+
     </div>
   );
 }
